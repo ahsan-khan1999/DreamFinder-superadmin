dcaa23618e2eaf17f69631c363916c0eaeb989d1
Sohaib <sohaib@rippleuae.com>
2021-11-06 09:48:03 +0500

    Changes in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,6 +19,7 @@ export function configureStore(initialState) {
   const store = createStore(
     reducers,
     initialState,
+    applyMiddleware(thunk)
     // composeEnhancers(applyMiddleware(thunk))
   );
 
