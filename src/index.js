d89208f74ad773f1feb136f34fe8e5cde30aec21
Ahsan <ahsan@rippleuae.com>
2021-11-20 10:27:25 +0500

    Changes in index.js app renderer replaced

--- src/index.js
+++ src/index.js
@@ -8,42 +8,18 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'react-image-lightbox/style.css';
 import 'video.js/dist/video-js.css';
-// import { defaultColor } from './constants/defaultValues';
-// import { setCurrentColor } from './helpers/Utils';
-
-//  const color = defaultColor
-// //   isMultiColorActive || isDarkSwitchActive ? getCurrentColor() : defaultColor;
-// // alert(defaultColor);
-
-// setCurrentColor(color);
-
-// const render = () => {
-//   import(`./assets/css/sass/themes/gogo.${color}.scss`).then(() => {
-//     require('./AppRenderer');
-//   });
-// };
-// render();
-
-
-import React from 'react'
-import { Suspense } from 'react';
 import { defaultColor } from './constants/defaultValues';
 import { setCurrentColor } from './helpers/Utils';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { configureStore } from './redux/store';
-const color = defaultColor;
-import "./assets/css/sass/themes/gogo.light.bluenavy.scss"
+
+ const color = defaultColor
+//   isMultiColorActive || isDarkSwitchActive ? getCurrentColor() : defaultColor;
+// alert(defaultColor);
+
 setCurrentColor(color);
 
-const App = React.lazy(() => import(/* webpackChunkName: "App" */ './App'));
-const Main = () => {
-  return (
-    <Provider store={configureStore()}>
-      <Suspense fallback={<div className="loading" />}>
-        <App />
-      </Suspense>
-    </Provider>
-  );
+const render = () => {
+  import(`./assets/css/sass/themes/gogo.${color}.scss`).then(() => {
+    require('./AppRenderer');
+  });
 };
-ReactDOM.render(<Main />, document.getElementById('root'));
+render();
